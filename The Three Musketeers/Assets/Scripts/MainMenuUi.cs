07ea880f0626eb46fcb84580314a96423f45cbad
Hirepp <vonwelcksarah@gmail.com>
2026-05-19 12:53:53 +0200

    Main Menu
    Some Buttons of the Main Menu

---
+++ The Three Musketeers/Assets/Scripts/MainMenuUi.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    // Called when Play is pressed
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("BookScene");
+    }
+
+    // Open settings menu
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // Back to main menu
+    public void BackToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}

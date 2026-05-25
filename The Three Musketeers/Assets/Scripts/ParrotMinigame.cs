ed9e206a216c269631d1e5939732e696533068e6
Muidt <vdduimtijmen@gmail.com>
2026-05-25 17:38:50 +0200

    character and parrot animation
    animation implementation. moms spaghetti got vomit on my sweater already. noobs

--- The Three Musketeers/Assets/Scripts/ParrotMinigame.cs
+++ The Three Musketeers/Assets/Scripts/ParrotMinigame.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-
 public class ParrotMinigame : MonoBehaviour
 {
     public Transform[] waypoints;
@@ -9,34 +8,55 @@ public class ParrotMinigame : MonoBehaviour
     private int currentWaypoint = 0;
 
     private bool isFlying = false;
-
-    void Update()
+    private Animator animator;
+    void Start()
     {
-        if (!isFlying) return;
-
-        Transform target = waypoints[currentWaypoint];
-
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            target.position,
-            flySpeed * Time.deltaTime
-        );
+        animator = GetComponent<Animator>();
     }
+    void Update()
+{
+    if (!isFlying) return;
+
+    Transform target = waypoints[currentWaypoint];
+
+    transform.position = Vector3.MoveTowards(
+        transform.position,
+        target.position,
+        flySpeed * Time.deltaTime
+    );
+
+    float distance = Vector3.Distance(transform.position, target.position);
+
+    if (distance < 0.1f)
+    {
+        isFlying = false;
+        animator.SetBool("IsFlying", false);
+
+        Debug.Log("Reached waypoint");
+    }
+}
 
     public void StartMinigame()
     {
         currentWaypoint = 1;
         isFlying = true;
+        animator.SetBool("IsFlying", true);
+        animator.SetTrigger("TakeOff");
     }
 
     public void AdvanceWaypoint()
-    {
-        currentWaypoint++;
+{
+    currentWaypoint++;
+    isFlying = true;
+    animator.SetBool("IsFlying", true);
 
-        if (currentWaypoint >= waypoints.Length)
-        {
-            Debug.Log("Parrot caught!");
-            isFlying = false;
-        }
+    if (currentWaypoint >= waypoints.Length)
+    {
+        Debug.Log("Parrot caught!");
+
+        isFlying = false;
+
+        animator.SetBool("IsFlying", false);
     }
 }
+    }

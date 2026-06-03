1160c75595cc849d38010f48b5595d1d1683ac9c
HarvestofRye <vanwesten.riley@gmail.com>
2026-06-03 21:56:25 +0200

    signs work with E
    YURR

---
+++ The Three Musketeers/Assets/Scripts/Sign Inspect/PosterInspect.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PosterInspect : MonoBehaviour
+{
+    public GameObject gameplayCamera;
+    public GameObject posterCam;
+    public PlayerMovement playerMovement;
+    public GameObject playerModel;
+    
+    public GameObject interactPrompt;
+    
+    private bool playerInRange = false;
+    private bool SeePoster = false;
+
+    private void Start()
+    {
+    }
+    
+    void Update()
+    {
+        if (playerInRange && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            SeePoster = !SeePoster;
+
+            gameplayCamera.SetActive(!SeePoster);
+            posterCam.SetActive(SeePoster);
+
+            playerMovement.enabled = !SeePoster;
+            
+            //hide player while seeing poster
+            playerModel.SetActive(!SeePoster);
+            
+            // Hide prompt while reading
+            interactPrompt.SetActive(!SeePoster);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGER WORKED");
+
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            
+            if (SeePoster)
+            {
+                SeePoster = false;
+                
+                gameplayCamera.SetActive(true);
+                posterCam.SetActive(false);
+                
+                playerMovement.enabled = true;
+                playerModel.SetActive(true);
+            }
+        }
+    }
+}
